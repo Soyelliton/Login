6d213673d6ad6cf11896a6822b6422a70d293509
Soyelliton <soyelliton@gmail.com>
2022-05-02 01:00:42 +0530

    adding validations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Title } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  Title = 'login';
+export class AppComponent implements OnInit {
+  Title = 'login'
+  loginform = new FormGroup({
+    password: new FormControl('', [Validators.required]),
+  });
+  
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  get password() {
+    return this.loginform.get('password');
+  }
+
 }
